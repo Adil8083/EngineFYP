eb7e20df5ebf6591e150f7a9da4cd16310e76c52
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2020-11-20 00:46:29 +0500

    Fixed the gitignore files

--- Start.js
+++ Start.js
@@ -1,23 +1,55 @@
-const expres = require("expres");
-const app = expres();
-
+const express = require("express");
+const app = express();
 const { spawn } = require("child_process");
 
-const main = () => {
-  const ls = spawn("./test.sh", ["hello123", "sedxed3"]);
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + "/image/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, "test.png");
+  },
+});
+
+var upload = multer({
+  storage: storage,
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + " is starting ...");
+  },
+});
+
+const main = (appName, userId) => {
+  const ls = spawn("./test.sh", [appName, userId]);
   ls.stdout.on("data", (data) => {
     console.log(`stdout: ${data}`);
+    return data;
   });
   ls.stderr.on("data", (data) => {
     console.error(`stderr: ${data}`);
+    return data;
+  });
+  ls.on("error", (error) => {
+    console.error(`stderr: ${error}`);
+    return error;
   });
   ls.on("close", (code) => {
     console.log(`child process exited with code ${code}`);
+    return code;
   });
 };
 
-main();
-const port = 3000;
+app.post("/", upload.single("appIcon"), async (req, res) => {
+  let request = await main(req.body.name, req.body.userId);
+  res.status(200).send({
+    success: true,
+    message:
+      "Your app is under construciton, you will receive the url to download your app in a while",
+  });
+});
+
+const port = 8888;
 app.listen(port, () => {
-  console.log("running server at port ", 3000);
+  console.log("running server at port ", 8888);
 });
--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 echo "app_name="$1"">.env
 echo "USER_ID=$2">./testWhiteLabel/.env
+
 node index.js
+
+echo "BEFORE SLEEPING"
+sleep 5
+echo "AFTER SLEEPING"
+
 cd testWhiteLabel
-cd android
-./gradlew assembleRelease
+yarn build
+
+# mv testWhiteLabel/android/app/build/outputs/apk/release/app-release.apk ./build
+echo "Your build has been successfully generated!!!"
