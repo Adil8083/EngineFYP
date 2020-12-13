be46542bc909a8fd25047c409bd5e898ce5167d7
uzirnaseem <uzair12naseem@gmail.com>
2020-12-13 16:22:05 +0500

    added basic navigations

--- testWhiteLabel/App.js
+++ testWhiteLabel/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native';
+import DrawerNavigation from './Navigations/DrawerNavigation';
 import Login from './screens/Login';
 export default function App() {
-  return <Login />;
+  return (
+    <NavigationContainer>
+      <DrawerNavigation />
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({});
