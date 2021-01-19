e5b2e1b1a5cac3e0a8375e9f2171d1d6b5a0da30
Adil Waheed <adilwahed@outlook.com>
2021-01-19 18:04:05 +0500

    fix actor bio navigation

--- testWhiteLabel/Navigations/DrawerNavigation.js
+++ testWhiteLabel/Navigations/DrawerNavigation.js
@@ -5,6 +5,7 @@ import Header from '../components/common/header';
 import ActorMain from '../screens/Actor/ActorMain';
 import DrawerContent from '../screens/DrawerContent';
 import Home from '../screens/Home';
+import ActorBio from '../screens/Actor/ActorBio';
 import ConcertDetails from '../screens/Singer/ConcertDetails';
 import Gallery from '../screens/Singer/Gallery';
 import MyPost from '../screens/MyPost';
@@ -26,6 +27,7 @@ export default function DrawerNavigation() {
         <Drawer.Screen name="Concert" component={ConcertDetails} />
         <Drawer.Screen name="Gallery" component={Gallery} />
         <Drawer.Screen name="MyPost" component={MyPost} />
+        <Drawer.Screen name="ActorBio" component={ActorBio} />
       </Drawer.Navigator>
     </>
   );
--- testWhiteLabel/screens/Actor/ActorBio.js
+++ testWhiteLabel/screens/Actor/ActorBio.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import AppText from '../../components/common/AppText';
 import data from '../../Data.json';
 import colors from '../../Theme/colors';
@@ -8,95 +8,117 @@ import AppView from '../../components/common/AppView';
 import TextSize from '../../components/common/TextSize';
 import Bio from '../../components/Actor/Bio';
 import Hobbies from '../../components/Actor/Hobbies';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-export default function ActorBio() {
+export default function ActorBio({navigation: {goBack}}) {
   // const [openMovies, setOpenMovies] = useState(true);
   // const [openEdu, setOpenEdu] = useState(true);
   return (
-    <AppView ViewStyle={{}}>
+    <>
       <View
         style={{
-          height: 40,
-          backgroundColor: colors.primary,
-          borderTopLeftRadius: 10,
-          borderTopRightRadius: 10,
-          justifyContent: 'center',
+          flexDirection: 'row',
+          paddingVertical: 10,
           alignItems: 'center',
-          // flexDirection: 'row',
-          padding: 10,
-          marginTop: 10,
-        }}>
-        <AppText
-          styleText={{
-            color: colors.TextColor,
-            fontWeight: '100',
-            fontSize: TextSize.SubHeading,
-          }}>
-          PERSONAL BIO
-        </AppText>
-      </View>
-      <Animatable.View
-        animation="slideInDown"
-        style={{
-          backgroundColor: colors.white,
-          height: 250,
-          borderBottomRightRadius: 5,
-          borderBottomLeftRadius: 5,
-          justifyContent: 'center',
-          marginBottom: 5,
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 0,
-          },
-          shadowOpacity: 1,
-
-          elevation: 20,
-        }}>
-        <Bio />
-      </Animatable.View>
-      <View
-        style={{
-          height: 40,
+          paddingLeft: 20,
           backgroundColor: colors.primary,
-          borderTopLeftRadius: 10,
-          borderTopRightRadius: 10,
-          justifyContent: 'center',
-          alignItems: 'center',
-          // flexDirection: 'row',
-          padding: 10,
-          marginTop: 10,
         }}>
-        <AppText
-          styleText={{
-            color: colors.TextColor,
-            fontWeight: '100',
-            fontSize: TextSize.SubHeading,
-          }}>
-          HOBBIES
-        </AppText>
+        <TouchableOpacity
+          style={{width: '10%', alignItems: 'center'}}
+          onPress={() => goBack()}>
+          <AntDesign name="arrowleft" size={20} color={colors.secondary} />
+        </TouchableOpacity>
+        <View style={{width: '80%', alignItems: 'center'}}>
+          <AppText styleText={{fontSize: TextSize.Heading}}>
+            {data.AppName}
+          </AppText>
+        </View>
       </View>
-      <Animatable.View
-        animation="slideInDown"
-        style={{
-          backgroundColor: colors.white,
-          height: 250,
-          borderBottomRightRadius: 5,
-          borderBottomLeftRadius: 5,
-          justifyContent: 'center',
-          marginBottom: 5,
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 0,
-          },
-          shadowOpacity: 1,
+      <AppView ViewStyle={{}}>
+        <View
+          style={{
+            height: 40,
+            backgroundColor: colors.primary,
+            borderTopLeftRadius: 10,
+            borderTopRightRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            // flexDirection: 'row',
+            padding: 10,
+            marginTop: 10,
+          }}>
+          <AppText
+            styleText={{
+              color: colors.TextColor,
+              fontWeight: '100',
+              fontSize: TextSize.SubHeading,
+            }}>
+            PERSONAL BIO
+          </AppText>
+        </View>
+        <Animatable.View
+          animation="slideInDown"
+          style={{
+            backgroundColor: colors.white,
+            height: 250,
+            borderBottomRightRadius: 5,
+            borderBottomLeftRadius: 5,
+            justifyContent: 'center',
+            marginBottom: 5,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 0,
+            },
+            shadowOpacity: 1,
 
-          elevation: 20,
-        }}>
-        <Hobbies hobbies={data.hobbies} />
-      </Animatable.View>
-    </AppView>
+            elevation: 20,
+          }}>
+          <Bio />
+        </Animatable.View>
+        <View
+          style={{
+            height: 40,
+            backgroundColor: colors.primary,
+            borderTopLeftRadius: 10,
+            borderTopRightRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            // flexDirection: 'row',
+            padding: 10,
+            marginTop: 10,
+          }}>
+          <AppText
+            styleText={{
+              color: colors.TextColor,
+              fontWeight: '100',
+              fontSize: TextSize.SubHeading,
+            }}>
+            HOBBIES
+          </AppText>
+        </View>
+        <Animatable.View
+          animation="slideInDown"
+          style={{
+            backgroundColor: colors.white,
+            height: 250,
+            borderBottomRightRadius: 5,
+            borderBottomLeftRadius: 5,
+            justifyContent: 'center',
+            marginBottom: 5,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 0,
+            },
+            shadowOpacity: 1,
+
+            elevation: 20,
+          }}>
+          <Hobbies hobbies={data.hobbies} />
+        </Animatable.View>
+      </AppView>
+    </>
   );
 }
 
